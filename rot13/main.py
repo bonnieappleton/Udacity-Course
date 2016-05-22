895182e8c28e7b314bee8a844831d4bab8798136
Bonnie Appleton <bemappleton@hotmail.com>
2016-05-22 17:13:33 +0100

    Minor edits to main.py to clean up code and removal of css file

--- rot13/main.py
+++ rot13/main.py
@@ -25,23 +25,23 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 
 
-class Handler(webapp2.RequestHandler) :
+class Handler(webapp2.RequestHandler):
     #write to browser
-    def write(self, *a, **kw) :
+    def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
     #get template
-    def render_str(self, template, **params) :
+    def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
 
     #write template to browser
-    def render(self, template, **kw) :
+    def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class MainHandler(Handler) :
+class MainHandler(Handler):
 
-    def write_form(self, **params) :
+    def write_form(self, **params):
         self.render("rot_13.html", **params)
         #self.response.out.write(form % {"error": error, "text": text})
 
@@ -51,26 +51,26 @@ class MainHandler(Handler) :
     def post(self):
         user_input = self.request.get("text")
 
-        if user_input :
+        if user_input:
             text = rot_text(user_input)
             escaped_text = cgi.escape(text, quote=True)
 
             self.write_form(error = "Yep that works.", text = escaped_text)
-        else :
+        else:
             self.write_form(error = "Enter some text first!", text = "")
 
 #Computes ROT13 value of input
-def rot_text(text) :
+def rot_text(text):
     new_text = ""
     for character in text:
-        if character.isalpha() :
+        if character.isalpha():
             character_order = ord(character) + 13
             #Wrap the letters for uppercase and lowercase letters which are at different character orders
-            if character.isupper() :
+            if character.isupper():
                 if character_order > 90:
                     character_order = character_order - 26
             else:
-                if character_order > 122 :
+                if character_order > 122:
                     character_order = character_order - 26
             character = chr(character_order)
         new_text = new_text + character
